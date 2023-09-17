cc90f6628e4a178ba3a17be21fe2f70bb45016d8
Vladimir Fibe <motiw@icloud.com>
2023-09-17 15:45:50 +0600

    Initial Commit

---
+++ Snake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by Vladimir Fibe on 17.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

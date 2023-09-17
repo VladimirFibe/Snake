5a32979f0f80e45fea3e5d7aa0675e8b5156d474
Vladimir Fibe <motiw@icloud.com>
2023-09-17 15:49:29 +0600

    Start

--- Snake/ViewController.swift
+++ Snake/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  Snake
-//
-//  Created by Vladimir Fibe on 17.09.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
